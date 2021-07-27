c968309bacf7e05daac5610ef3a78f525d54147a
Artur Barilo <faustkeller45@gmail.com>
2021-07-28 00:11:24 +0300

    master | wip solving first task

--- src/easy/1TwoSum.cpp
+++ src/easy/1TwoSum.cpp
@@ -15,16 +15,12 @@ public:
 	std::vector<int> twoSum(std::vector<int>& nums, int target) {
 		auto vec = std::vector<int>();
 		for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
-			if (vec.empty() && nums[i] < target) {
-				vec.push_back(i);
-			} else {
-				for (auto key : vec) {
-					if (nums[static_cast<size_t>(key)] + nums[i] == target) {
-						return std::vector<int>({key, i});
-					}
+			for (const auto& key : vec) {
+				if (nums[static_cast<size_t>(key)] + nums[i] == target) {
+					return std::vector<int>({key, i});
 				}
-				vec.push_back(i);
 			}
+			vec.push_back(i);
 		}
 
 		return std::vector<int>();
@@ -37,20 +33,20 @@ TEST_F(solutionClass, twoSum) {
 	auto example1 = std::vector<int>({2, 7, 11, 15});
 	auto result1 = twoSum(example1, 9);
 	auto expect1 = std::vector<int>({0, 1});
-	EXPECT_EQ(twoSum(example1, 9), std::vector<int>({0, 1}));
+	EXPECT_TRUE((result1 == expect1) || (result1[0] == expect1[1] && result1[1] == expect1[0]));
 
 	auto example2 = std::vector<int>({3, 2, 4});
 	auto result2 = twoSum(example2, 6);
 	auto expect2 = std::vector<int>({1, 2});
-	EXPECT_EQ(twoSum(example2, 6), std::vector<int>({1, 2}));
+	EXPECT_TRUE((result2 == expect2) || (result2[0] == expect2[1] && result2[1] == expect2[0]));
 
 	auto example3 = std::vector<int>({3, 2, 4});
-	auto result3 = twoSum(example3, 6);
-	auto expect3 = std::vector<int>({0, 1});
-	EXPECT_EQ(twoSum(example3, 6), std::vector<int>({0, 1}));
+	auto result3 = twoSum(example3, 7);
+	auto expect3 = std::vector<int>({0, 2});
+	EXPECT_TRUE((result3 == expect3) || (result3[0] == expect3[1] && result3[1] == expect3[0]));
 
 	auto example4 = std::vector<int>({54, 45, 2, 5, 3, 5, 4});
 	auto result4 = twoSum(example4, 6);
 	auto expect4 = std::vector<int>({2, 6});
-	EXPECT_EQ(twoSum(example4, 6), std::vector<int>({2, 6}));
+	EXPECT_TRUE((result4 == expect4) || (result4[0] == expect4[1] && result4[1] == expect4[0]));
 }
