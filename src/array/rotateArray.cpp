87300d5caefee4e9710405322b59cf913b174546
Artur Barilo <faustkeller45@gmail.com>
2021-11-29 22:57:18 +0300

    master | wip: solving problems with arrays

---
+++ src/array/rotateArray.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Artur Barilo on 29.11.2021.
+//
+#include <gtest/gtest.h>
+#include <vector>
+
+class rotateArrayClass : public testing::Test {
+public:
+    void rotateArray(std::vector<int>& nums, int k) {
+        std::vector<int> items(nums.end() - std::min<int>(k, nums.size()), nums.end());
+        nums.resize(nums.size() - k);
+        nums.insert(nums.begin(), items.begin(), items.end());
+    }
+};
+
+struct sTest {
+    std::vector<int> example;
+    int k;
+    std::vector<int> result;
+    sTest(const std::vector<int>& _e, int _k, const std::vector<int>& _r) : example(_e), k(_k), result(_r) {}
+};
+
+TEST_F(rotateArrayClass, rotateArray) {
+    std::vector<sTest> cases = {
+        {{1,2}, 3, {2,1}},
+        {{1,2,3,4,5,6,7}, 3, {5,6,7,1,2,3,4}},
+        {{1,2,3,4,5,6,7}, 1, {7,1,2,3,4,5,6}},
+        {{1,2,3,4,5,6,7}, 2, {6,7,1,2,3,4,5}},
+
+        {{-1,-100,3,99}, 2, {3,99,-1,-100}},
+        {{-1,-100,3,99}, 1, {99,-1,-100,3}}
+    };
+
+    for (auto& item : cases) {
+        rotateArray(item.example, item.k);
+        EXPECT_EQ(item.example, item.result);
+    }
+}
